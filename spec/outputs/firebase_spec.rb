b4f08fe270af31d3ecf7d6b3b2c29750bed6ec8f
Valentin Crettaz <valentin.crettaz@consulthys.com>
2017-01-12 16:35:24 +0100

    Update firebase_spec.rb

--- spec/outputs/firebase_spec.rb
+++ spec/outputs/firebase_spec.rb
@@ -1,11 +1,22 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/outputs/firebase"
+require "logstash/codecs/plain"
+require "logstash/event"
 
 describe LogStash::Outputs::Firebase do
+  let(:sample_event) { LogStash::Event.new }
+  let(:output) { LogStash::Outputs::Firebase.new }
 
-  it_behaves_like "an interruptible output plugin" do
-    let(:config) { { "interval" => 100 } }
+  before do
+    output.register
   end
 
+  describe "receive message" do
+    subject { output.receive(sample_event) }
+
+    it "returns a string" do
+      expect(subject).to eq("Event received")
+    end
+  end
 end
